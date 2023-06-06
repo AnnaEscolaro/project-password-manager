f0d0eea5c4dc7045084ae3c62826a24644f20c52
Anna Escolaro <annaescolaro@hotmail.com>
2023-06-06 20:01:49 -0300

    req 4

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,64 @@
+import { useState } from 'react';
+
 type Props = {
   setCreatePassword:(arg:boolean) => void;
 };
 
 function CreateForm(props: Props) {
+  const [password, setPassword] = useState('');
+
+  const [service, setService] = useState('');
+
+  const [login, setLogin] = useState('');
+
   const { setCreatePassword } = props;
+
+  // VALIDATING PASSWORD
+  const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    const passwordValue = event.target.value;
+    setPassword(passwordValue);
+  };
+
+  const handleRegisterPassword = () => {
+    // const regex = /^[a-zA-Z0-9!@#$%^&*)(+=._-]+$/g;
+    const regexLetters = /[a-zA-Z]+/;
+    const regexNumbers = /\d+/g;
+    const regexSpecialCharacters = /[ !"#$%&'()*+,-./:;<=>?@[\\\]^_`{|}~]/;
+    return !(password.length >= 8
+      && password.length <= 16
+      && regexLetters.test(password)
+      && regexNumbers.test(password)
+      && regexSpecialCharacters.test(password));
+  };
+
+  // VALIDATING SERVICE
+  const handleService = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    const serviceValue = event.target.value;
+    setService(serviceValue);
+  };
+
+  const handleRegisterService = () => {
+    return (service.length < 1);
+  };
+
+  // VALIDATING LOGIN
+  const handleLogin = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    const loginValue = event.target.value;
+    setLogin(loginValue);
+  };
+
+  const handleRegisterLogin = () => {
+    return (login.length < 1);
+  };
+
+  // VALIDATING BUTTON ACTIVATION
+  const buttonActivation = () => {
+    console.log(handleRegisterPassword());
+    return (handleRegisterPassword() || handleRegisterService() || handleRegisterLogin());
+  };
 
   return (
     <div>
@@ -11,31 +66,31 @@ function CreateForm(props: Props) {
         <label htmlFor="name">
           Nome do serviço
           <input
-            // onChange={}
-            // value={}
+            onChange={ handleService }
+            value={ service }
             type="text"
             id="name"
-            // required
+            required
           />
         </label>
         <label htmlFor="login">
           Login
           <input
-            // onChange={}
-            // value={}
+            onChange={ handleLogin }
+            value={ login }
             type="text"
             id="login"
-            // required
+            required
           />
         </label>
         <label htmlFor="senha">
           Senha
           <input
-            // onChange={}
-            // value={}
+            onChange={ handlePassword }
+            value={ password }
             type="password"
             id="senha"
-            // required
+            required
           />
         </label>
         <label htmlFor="URL">
@@ -45,11 +100,10 @@ function CreateForm(props: Props) {
             // value={}
             type="text"
             id="URL"
-            // required
           />
         </label>
       </form>
-      <button>Cadastrar</button>
+      <button disabled={ buttonActivation() }>Cadastrar</button>
       <button onClick={ () => setCreatePassword(false) }>Cancelar</button>
     </div>
   );
